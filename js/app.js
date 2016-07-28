47ab28022cdaf1bbf5f9d247262a311797c40547
David Smith <dbsmith.dbs83@gmail.com>
2016-07-28 09:48:53 -0700

    comments, removed token reference

--- js/app.js
+++ js/app.js
@@ -27,20 +27,6 @@
   Entry.reposObj.myRepos = [];
 
   Entry.reposObj.requestRepos = function(nextFunction1, nextFunction2) {
-    // $.get({'/github/users/Bl41r/repos' +
-    //      '?per_page=0' +
-    //      '&sort=update',
-    //   type: 'GET',
-    //   headers: {
-    //     'Authorization': 'token ' + githubToken,
-    //   },
-    //   success: function(data, message, xhr) {
-    //     Entry.reposObj.myRepos = data;
-    //     console.log(data);
-    //     localStorage.myPortProjectRepos = JSON.stringify(data);
-    //     nextFunction1(nextFunction2);
-    //   }
-    // });
     $.get('/github/users/Bl41r/repos?per_page=0&sort=update')
           .done(function(data) {
             Entry.reposObj.myRepos = data;
